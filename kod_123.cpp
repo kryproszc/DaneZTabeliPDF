70154d5d6022e9227936ebb43f135fe9df624707
kryproszc <62724175+kryproszc@users.noreply.github.com>
2024-03-15 23:39:04 +0100

    Add files via upload

--- kod_123.cpp
+++ kod_123.cpp
@@ -1,29 +1,6 @@
-auto start = std::chrono::high_resolution_clock::now();
-
-
-
-
-
-  
-  auto end = std::chrono::high_resolution_clock::now();
-std::chrono::duration<double> elapsed = end - start;
-Rcpp::Rcout << "ThreadPool time: " << elapsed.count() << " s\n";
-
-
-
-
-
-
-
-
-start = std::chrono::high_resolution_clock::now();
-
-
-
-
-wywolanie funkcji  iteracyjnej
-  
-  
-  end = std::chrono::high_resolution_clock::now();
-elapsed = end - start;
-Rcpp::Rcout << "Iterative time: " << elapsed.count() << " s\n";
+for (const auto& threadData : threadDataVec) {
+  lat_sub.insert(lat_sub.end(), threadData.lat_sub.begin(), threadData.lat_sub.end());
+  lon_sub.insert(lon_sub.end(), threadData.lon_sub.begin(), threadData.lon_sub.end());
+  insu_sub.insert(insu_sub.end(), threadData.insu_sub.begin(), threadData.insu_sub.end());
+  reas_sub.insert(reas_sub.end(), threadData.reas_sub.begin(), threadData.reas_sub.end());
+  premium_sub.insert(premium_sub.end(), threadData.premium_sub.begin(), threadData.premium_sub.end());
