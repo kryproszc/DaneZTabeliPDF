f95a73074d7bdd26f210025b3fb6149a8fe07c28
kryproszc <62724175+kryproszc@users.noreply.github.com>
2024-03-15 21:45:58 +0100

    Add files via upload

--- kod_123.cpp
+++ kod_123.cpp
@@ -1,13 +1,6 @@
-
-
-// [[Rcpp::export]]
-void testIterativeFactorial()
-{
-  const size_t size = 20; 
-  std::vector<unsigned long long> results(size);
-  
-  
-  for (size_t i = 0; i < size; ++i) {
-    results[i] = factorial(i + 10);
-  }
+unsigned long long factorial(unsigned int n) {
+  if (n == 0)
+    return 1;
+  else
+    return n * factorial(n - 1);
 }
